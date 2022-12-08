3c6e03bb3dc614675104fd4d22be9b0165a1108a
Brilianmadiuw <117911653+Brilianmadiuw@users.noreply.github.com>
2022-12-08 16:17:25 +0700

    Add files via upload

---
+++ 2_D_71220931.py
@@ -0,0 +1,23 @@
+print("Pilihan Model matematika")
+print("1. Perkalian")
+print("2. Pembagian")
+a = input("Masukkan model matematika yang diinginkan: ")
+b = input("Menampilkan table matematika dari angka: ")
+
+
+numbers = [1, 2, 3, 4, 5 ,6, 7, 8, 9, 10]
+sum = 0
+for i in numbers:
+    sum = i
+    print(b, "x", sum, "=", b*sum )
+
+print("Pilihan Model matematika")
+print("1. Perkalian")
+print("2. Pembagian")
+a = input("Masukkan model matematika yang diinginkan: ")
+b = input("Menampilkan table matematika dari angka: ")
+numbers = [50, 51, 52, 53, 54, 55 ,56, 57, 58, 59, 60, 61, 62, 63, 64, 65]
+sum = 0
+for i in numbers:
+    sum = i
+    print(b, ":", sum, "=", b/sum )
